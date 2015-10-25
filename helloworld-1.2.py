8b8c695d780dc0b67a409546b2e51f950e42cf39
Lbusi96 <louisebusinge@gmail.com>
2015-10-25 17:58:05 +0000

    Create helloworld-1.2.py

---
+++ helloworld-1.2.py
@@ -0,0 +1,9 @@
+#program asking for the users name producing an output how are you?
+
+username= ""
+
+username= input ("What is your username? ")
+
+print ("Hello, " + username + " how are you? ")
+
+
